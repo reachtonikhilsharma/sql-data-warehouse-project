33cfebfb1a88df6c7a934660d9b22b415e885da7
reachtonikhilsharma <reachto.nikhilsharma@gmail.com>
2026-06-24 05:45:00 +0530

    Update ddl_silver.sql

--- script/silver/ddl_silver.sql
+++ script/silver/ddl_silver.sql
@@ -25,6 +25,8 @@ if object_id ('silver.crm_prd_info', 'u') is not null
 create table silver.crm_prd_info (
 	prd_id int,
 	prd_key nvarchar(50),
+	cat_id nvarchar(50),
+	sls_prd_key nvarchar(50),
 	prd_nm nvarchar(50),
 	prd_cost int,
 	prd_line nvarchar(50),
